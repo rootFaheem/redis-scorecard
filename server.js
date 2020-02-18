7dc07e4f92675d14d5b61c01727934f7a7d96956
rootFaheem <faheemtech1@gmail.com>
2020-02-19 00:50:25 +0530

    data fetched from cricket api

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
 
+const axios = require("axios");
+
 const PORT = process.env.PORT || 8080;
 const CRICKET_API_TOKEN = process.env.CRICKET_API_TOKEN;
 const MATCH_KEY = process.env.MATCH_KEY;
 
 app.get("/", (req, res, next) => res.send(200));
 
-const REQ_URL = `https://rest.cricketapi.com/rest/v2/match/${MATCH_KEY}/?access_token=${CRICKET_API_TOKEN}`;
+app.get("/api/score", async (req, res, next) => {
+  try {
+    const { data: data } = await axios.get(
+      `https://rest.cricketapi.com/rest/v2/match/${MATCH_KEY}/?access_token=${CRICKET_API_TOKEN}`
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "successful",
+      data
+    });
+  } catch (error) {
+    return error;
+  }
+});
 
 app.listen(PORT, () => console.log(`catch server at: ${PORT}`));
