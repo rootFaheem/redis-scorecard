4de08bd1f5a79c650aae2d00abe6b39005a7bb78
rootFaheem <faheemtech1@gmail.com>
2020-02-18 23:39:29 +0530

    /api test

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ const app = express();
 const PORT = process.env.PORT;
 const CRICKET_API_TOKEN = process.env.CRICKET_API_TOKEN;
 
+app.get("/", (req, res, next) => res.send(200));
+
 app.listen(PORT, () => console.log(`catch server at: ${PORT}`));
