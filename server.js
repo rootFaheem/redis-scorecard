89074d0783d4a0e7972472fe0a943e639352f400
rootFaheem <faheemtech1@gmail.com>
2020-02-19 23:25:13 +0530

    response from the cricket api

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ const express = require("express");
 const app = express();
 
 const axios = require("axios");
+const redis = require("redis");
 
 const PORT = process.env.PORT || 8080;
 const CRICKET_API_TOKEN = process.env.CRICKET_API_TOKEN;
 const MATCH_KEY = process.env.MATCH_KEY;
+const REDIS_PORT = process.env.REDIS_PORT;
+
+const client = redis.createClient(REDIS_PORT);
 
 app.get("/", (req, res, next) => res.send(200));
 
 app.get("/api/score", async (req, res, next) => {
   try {
-    const { data: data } = await axios.get(
+    const {
+      data: { data }
+    } = await axios.get(
       `https://rest.cricketapi.com/rest/v2/match/${MATCH_KEY}/?access_token=${CRICKET_API_TOKEN}`
     );
 
@@ -26,3 +32,7 @@ app.get("/api/score", async (req, res, next) => {
 });
 
 app.listen(PORT, () => console.log(`catch server at: ${PORT}`));
+
+module.exports = {
+  client
+};
